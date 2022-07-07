833b6c8d26acabd85c102b9d3327d0b9c5ba7030
Shem-aiah <SINelson@student.fullsail.edu>
2022-07-06 21:04:39 -0400

    Updated Layout

--- CalculatorUI/calMain.cpp
+++ CalculatorUI/calMain.cpp
@@ -1,9 +1,77 @@
 #include "calMain.h"
+<<<<<<< HEAD
+wxBEGIN_EVENT_TABLE(calMain, wxFrame)
+wxEND_EVENT_TABLE()
+
+calMain::calMain() : wxFrame(nullptr, wxID_ANY, "Calculator UI!", wxPoint(50, 50), wxSize(335, 545))
+{
+	wxFont font(15, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false);
+	m_txt1 = new wxTextCtrl(this, wxID_ANY, "Test", wxPoint(0, 21), wxSize(322, 50));
+	CalButton = new wxButton(this, wxID_ANY, "Standard", wxPoint(0, 1), wxSize(80, 20));
+	CalButton = new wxButton(this, wxID_ANY, "Binary", wxPoint(79, 1), wxSize(80, 20));
+	CalButton = new wxButton(this, wxID_ANY, "Hex", wxPoint(159, 1), wxSize(80, 20));
+	CalButton = new wxButton(this, wxID_ANY, "Decimal", wxPoint(238, 1), wxSize(80, 20));
+	//Top Buttons
+	CalButton = new wxButton(this, wxID_ANY, "%", wxPoint(1, 70), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "CE", wxPoint(50, 70), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "C", wxPoint(100, 70), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "Del", wxPoint(150, 70), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "1/x", wxPoint(200, 70), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "x^2", wxPoint(250, 70), wxSize(70, 70));
+	CalButton->SetFont(font);
+	//Side Buttons
+	CalButton = new wxButton(this, wxID_ANY, "/", wxPoint(269, 156), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "X", wxPoint(269, 226), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "-", wxPoint(269, 296), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "+", wxPoint(269, 366), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "=", wxPoint(269, 436), wxSize(50, 70));
+	CalButton->SetFont(font);
+	//Num Pad
+	CalButton = new wxButton(this, wxID_ANY, "7", wxPoint(30, 156), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "8", wxPoint(90, 156), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "9", wxPoint(150, 156), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "4", wxPoint(30, 226), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "5", wxPoint(90, 226), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "6", wxPoint(150, 226), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "1", wxPoint(30, 296), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "2", wxPoint(90, 296), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "3", wxPoint(150, 296), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "+/-", wxPoint(30, 366), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, "0", wxPoint(90, 366), wxSize(50, 70));
+	CalButton->SetFont(font);
+	CalButton = new wxButton(this, wxID_ANY, ".", wxPoint(150, 366), wxSize(50, 70));
+	CalButton->SetFont(font);
+	//Grid
+	wxGridSizer* grid = new wxGridSizer(nFieldWidth, nFieldHeight, 0, 0);
+	this->SetSizer(grid);
+	grid->Layout();
+
+=======
 
 calMain::calMain() : wxFrame(nullptr, wxID_ANY, "Calculator UI!", wxPoint(50, 50), wxSize(335, 545)) 
 {
 	CalButton = new wxButton(this, wxID_ANY, "+", wxPoint(10, 70), wxSize(100, 30));
 	m_txt1 = new wxTextCtrl(this, wxID_ANY, "Test", wxPoint(10, 10), wxSize(300, 50));
+>>>>>>> 739d6e72dd581650230470bdc9e3816e430302d5
 }
 
 calMain::~calMain() 
