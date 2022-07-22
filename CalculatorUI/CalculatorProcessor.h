67179f365d5fafe3bcf9a7b411b798bf8bb8c537
Shem-aiah <SINelson@student.fullsail.edu>
2022-07-22 14:50:43 -0400

    CalculatorTest

--- CalculatorUI/CalculatorProcessor.h
+++ CalculatorUI/CalculatorProcessor.h
@@ -22,33 +22,3 @@ private:
 
 	static CalculatorProcessor s_Instance;
 };
-
-
-
-
-/*
-class CalculatorProcessor
-{
-static CalculatorProcessor *s_Instance;
-public:
-	CalculatorProcessor(const CalculatorProcessor&) = delete;
-
-	static CalculatorProcessor* Get()
-	{
-		if (s_Instance == false)
-		{
-			s_Instance = new CalculatorProcessor;
-		}
-		return s_Instance;
-	}
-
-	wxButton* CalButton() { return CalButtonM; }
-
-private:
-	CalculatorProcessor() {}
-
-	wxButton* CalButtonM = nullptr;
-
-
-};
-*/
